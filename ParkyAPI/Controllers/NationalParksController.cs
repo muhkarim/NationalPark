ca161be3b2c914aba62ec56575dd29d60c6cddd9
Muhammad Karim <muhammadkarim10@gmail.com>
2020-05-18 21:18:06 +0700

    Create API National Park, Trails, Add Swagger Documentations

---
+++ ParkyAPI/Controllers/NationalParksController.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using ParkyAPI.Models;
+using ParkyAPI.Models.Dtos;
+using ParkyAPI.Repository.IRepository;
+
+namespace ParkyAPI.Controllers
+{
+    //[Route("api/[controller]")]
+    [Route("api/v{version:apiVersion}/nationalparks")]
+    [ApiController]
+    //[ApiExplorerSettings(GroupName = "ParkyOpenAPISpecNP")]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)] //default response 
+    public class NationalParksController : Controller
+    {
+        private INationalParkRepository _nationalParkRepository;
+        private readonly IMapper _mapper;
+
+        public NationalParksController(INationalParkRepository nationalParkRepository, IMapper mapper)
+        {
+            _nationalParkRepository = nationalParkRepository;
+            _mapper = mapper;
+        }
+
+
+
+        /// <summary>
+        /// Get list of national parks
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [ProducesResponseType(200, Type = typeof(List<NationalParkDto>))]
+        public IActionResult GetNationalParks()
+        {
+            var objList = _nationalParkRepository.GetNationalParks();
+
+            var objDto = new List<NationalParkDto>();
+
+            // nampilin objDto dan menyembunyikan model aslinya(doamin model)
+            foreach (var obj in objList)
+            {
+                objDto.Add(_mapper.Map<NationalParkDto>(obj));
+            }
+
+            return Ok(objDto);
+
+        }
+
+        /// <summary>
+        /// Get individul national park 
+        /// </summary>
+        /// <param name="nationalParkId"> The Id of the National Park</param>
+        /// <returns></returns>
+        [HttpGet("{nationalParkId:int}", Name = "GetNationalPark")]
+        [ProducesResponseType(200, Type = typeof(NationalParkDto))]
+        [ProducesResponseType(404)]
+        [ProducesDefaultResponseType]
+        public IActionResult GetNationalPark(int nationalParkId)
+        {
+            var obj = _nationalParkRepository.GetNationalPark(nationalParkId);
+            if(obj == null)
+            {
+                return NotFound();
+            }
+
+            var objDto = _mapper.Map<NationalParkDto>(obj);
+
+            // kalo gapake automapper
+            //var objDto = new NationalParkDto()
+            //{
+            //    Created = obj.Created,
+            //    Id = obj.Id,
+            //    Name = obj.Name,
+            //    State = obj.State,
+            //};
+
+
+            return Ok(objDto);
+
+        }
+
+        [HttpPost]
+        [ProducesResponseType(201, Type = typeof(NationalParkDto))]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public IActionResult CreateNationalPark([FromBody] NationalParkDto nationalParkDto)
+        {
+
+            if(nationalParkDto == null)
+            {
+                return BadRequest(ModelState);
+            }
+
+            // jika nama sudah ada
+            if (_nationalParkRepository.NationalParkExists(nationalParkDto.Name)) // pakai method yg dibuat di repository dengan parameter name
+            {
+                ModelState.AddModelError("", "National Park Exist!");
+                return StatusCode(404, ModelState);
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            // convert domain model to dto
+            var nationalParkObj = _mapper.Map<NationalPark>(nationalParkDto);
+
+            if (!_nationalParkRepository.CreateNationalPark(nationalParkObj))
+            {
+                ModelState.AddModelError("", $"Something went wrong when saving the record {nationalParkObj.Name}");
+                return StatusCode(500, ModelState);
+            }
+
+            return CreatedAtRoute("GetNationalPark", new { version=HttpContext.GetRequestedApiVersion().ToString(),
+                nationalParkId = nationalParkObj.Id }, nationalParkObj); // 201 response
+
+        }
+
+
+
+        [HttpPatch("{nationalParkId:int}", Name = "UpdateNationalPark")]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public IActionResult UpdateNationalPark(int nationalParkId, [FromBody] NationalParkDto nationalParkDto)
+        {
+            if (nationalParkDto == null || nationalParkId != nationalParkDto.Id)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var nationalParkObj = _mapper.Map<NationalPark>(nationalParkDto);
+
+            if (!_nationalParkRepository.UpdateNationalPark(nationalParkObj))
+            {
+                ModelState.AddModelError("", $"Something went wrong when updating the record {nationalParkObj.Name}");
+                return StatusCode(500, ModelState);
+            }
+
+            return NoContent();
+
+        }
+
+        [HttpDelete("{nationalParkId:int}", Name = "DeleteNationalPark")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status409Conflict)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public IActionResult DeleteNationalPark(int nationalParkId)
+        {
+            if (!_nationalParkRepository.NationalParkExists(nationalParkId))
+            {
+                return NotFound();
+            }
+
+            var nationalParkObj = _nationalParkRepository.GetNationalPark(nationalParkId);
+
+            if (!_nationalParkRepository.DeleteNationalPark(nationalParkObj))
+            {
+                ModelState.AddModelError("", $"Something went wrong when deleting the record {nationalParkObj.Name}");
+                return StatusCode(500, ModelState);
+            }
+
+            return NoContent();
+
+        }
+
+
+
+
+
+
+    }
+}
---
+++ ParkyAPI/Controllers/NationalParksV2Controller.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using ParkyAPI.Models;
+using ParkyAPI.Models.Dtos;
+using ParkyAPI.Repository.IRepository;
+
+namespace ParkyAPI.Controllers
+{
+    //[Route("api/[controller]")]
+    [Route("api/v{version:apiVersion}/nationalparks")]
+    [ApiVersion("2.0")]
+    [ApiController]
+    //[ApiExplorerSettings(GroupName = "ParkyOpenAPISpecNP")]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)] //default response 
+    public class NationalParksV2Controller : Controller
+    {
+        private INationalParkRepository _nationalParkRepository;
+        private readonly IMapper _mapper;
+
+        public NationalParksV2Controller(INationalParkRepository nationalParkRepository, IMapper mapper)
+        {
+            _nationalParkRepository = nationalParkRepository;
+            _mapper = mapper;
+        }
+
+
+
+        /// <summary>
+        /// Get list of national parks.
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [ProducesResponseType(200, Type = typeof(List<NationalParkDto>))]
+        public IActionResult GetNationalParks()
+        {
+            var obj = _nationalParkRepository.GetNationalParks().FirstOrDefault();
+
+            return Ok(_mapper.Map<NationalParkDto>(obj));
+        }
+
+
+
+
+    }
+}
